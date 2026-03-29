cfacb40596bc0ec0c3f6bd5d9746b7d442495c93
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-29 22:51:25 +0500

    feat: firebase added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,54 @@
+import { Container } from '@mui/material'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { useContext } from 'react'
+import { Context } from '../index'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
 function Login() {
-    return <div>Login</div>
+    const { auth } = useContext(Context)
+
+    const login = async () => {
+        const provider = new GoogleAuthProvider()
+
+        try {
+            const result = await signInWithPopup(auth, provider)
+
+            const user = result.user
+            console.log('Kirgan foydalanuvchi:', user.displayName)
+        } catch (error) {
+            console.error('Kirishda xatolik:', error.code, error.message)
+        }
+    }
+
+    return (
+        <Container>
+            <Grid
+                container
+                alignItems={'center'}
+                justifyContent={'center'}
+                style={{ height: window.innerHeight - 50 }}
+            >
+                <Grid
+                    style={{ width: 400, backgroundColor: 'lightgray', borderRadius: 10 }}
+                    container
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                >
+                    <Box p={5}>
+                        <Button
+                            onClick={login}
+                            variant={'outlined'}
+                            style={{ color: '#222', borderColor: '#222' }}
+                        >
+                            Sign In With Google
+                        </Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
 
 export default Login
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
+import { initializeApp } from 'firebase/app'
+import 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const app = initializeApp({
+    apiKey: 'AIzaSyCtWMiKoauRgAZuN5Tgmoc9w2N0-8ebqn4',
+    authDomain: 'real-time-chat-4650b.firebaseapp.com',
+    projectId: 'real-time-chat-4650b',
+    storageBucket: 'real-time-chat-4650b.firebasestorage.app',
+    messagingSenderId: '550863214867',
+    appId: '1:550863214867:web:0a50b2cc2967432f7de2e6',
+    measurementId: 'G-89DPQC5YW7',
+})
+
+const auth = getAuth(app)
+const firestore = getFirestore(app)
+
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
+    <Context.Provider value={{ auth, firestore }}>
         <App />
-    </React.StrictMode>
+    </Context.Provider>
 )
